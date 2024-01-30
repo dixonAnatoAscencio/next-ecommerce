d3252659517a4b8b858a1456122f31e65b166602
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-01-30 12:14:48 -0500

    init ecommerce v1

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={titleFont.className}>Login Page</h1>
+    </div>
+  );
+}
